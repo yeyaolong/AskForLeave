f1d2573f4cb2b35720a39190c14b1a1f06a837ea
叶瑶隆 <470080502@qq.com>
2021-05-13 15:01:04 +0800

    咋黁

--- src/pages/form/components/beginLeave.tsx
+++ src/pages/form/components/beginLeave.tsx
@@ -8,13 +8,16 @@ import './beginLeave.less'
 
 
 
-import { UserInfo } from '../../../interface/index';
+import { UserInfo, Leave } from '../../../interface/index';
 
 interface MyProps {
     submitter: UserInfo,
     handleProxy: Function
 }
 interface MyState {
+  leave: Leave,
+  balance: number,
+  leaveList?: Array<Leave>
 }
 
 // 发起提交
@@ -23,6 +26,80 @@ export default class BeginLeave extends Component<MyProps, MyState> {
   constructor(props) {
     super(props);
     this.state = {
+      leave: {
+        rest: 3,
+        dispense: "手动发放",
+        type: "annual",
+        name: "年休假"
+      }
+      // leaveList: [
+      //   {
+      //     rest: 3,
+      //     dispense: "手动发放",
+      //     type: "annual",
+      //     name: "年休假"
+      //   },
+      //   {
+      //     rest: 3,
+      //     dispense: "手动发放",
+      //     type: "eventLeaveSalary",
+      //     name: "全薪事假"
+      //   },
+      //   {
+      //     rest: 3,
+      //     dispense: "手动发放",
+      //     type: "sickLeaveSalary",
+      //     name: "全薪病假"
+      //   },
+      //   {
+      //     rest: 3,
+      //     dispense: "手动发放",
+      //     type: "restLeave",
+      //     name: "调休"
+      //   },
+      //   {
+      //     rest: 3,
+      //     dispense: "手动发放",
+      //     type: "weddingLeave",
+      //     name: "婚假"
+      //   },
+      //   {
+      //     rest: 3,
+      //     dispense: "手动发放",
+      //     type: "maternityLeave",
+      //     name: "产假"
+      //   },
+      //   {
+      //     rest: 3,
+      //     dispense: "手动发放",
+      //     type: "paternityLeave",
+      //     name: "陪产假"
+      //   },
+      //   {
+      //     rest: 3,
+      //     dispense: "手动发放",
+      //     type: "bereavementLeave",
+      //     name: "丧假"
+      //   },
+      //   {
+      //     rest: 3,
+      //     dispense: "手动发放",
+      //     type: "eventLeave",
+      //     name: "事假"
+      //   },
+      //   {
+      //     rest: 3,
+      //     dispense: "手动发放",
+      //     type: "sickLeave",
+      //     name: "病假"
+      //   },
+      //   {
+      //     rest: 3,
+      //     dispense: "手动发放",
+      //     type: "obstetricsLeave",
+      //     name: "产检假"
+      //   },
+      // ]
     }
   }
 
@@ -40,11 +117,9 @@ export default class BeginLeave extends Component<MyProps, MyState> {
     //   代他人提交
     this.props.handleProxy();
   }
+  
 
   render () {
-    let icon = (
-      <View className='at-icon at-icon-chevron-right'></View>
-    )
     return (
         <View className='begin-leave'>
             
@@ -62,17 +137,18 @@ export default class BeginLeave extends Component<MyProps, MyState> {
             </View>            
             <View className='type cell'>
                 <View className='cell-label'>
+                    <View className='required'>*</View>
                     请假类型
                 </View>
-                {/* <View className='cell-value'>
-                    调休
-                </View> */}
-                <View className='cell-placeholder'>
-                    请选择
+                <View className={this.state.leave.type ? 'cell-value' : 'cell-placeholder'}>
+                 { this.state.leave.type ? this.state.leave.name : '请选择'}                 
                 </View>
                 <View className='suffix'>
                     <View className='at-icon at-icon-chevron-right'></View>
                 </View>
+            </View>
+            <View className='balance'>
+              假期余额：{ this.state.leave.rest }
             </View>
         </View>
     )
